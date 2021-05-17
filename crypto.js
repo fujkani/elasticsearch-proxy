37c36e55915a07f4cdd63be67b463d2fa1f876d3
jon ujkani <fujkani@hotmail.com>
2021-05-17 08:40:22 +0000

    Delete crypto.js

--- crypto.js
+++
@@ -1,8 +0,0 @@
-var crypto = require('crypto');
-
-const secret = 'abcdefg';
-const hash = crypto.createHmac('sha256', secret)
-                   .update('I love cupcakes')
-                   .digest('hex');
-
-console.log(hash);
